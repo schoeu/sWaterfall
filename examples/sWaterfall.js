a7a9ce091b32aed2f8ade5db80f82bb92424b506
schoeu <schoeu@163.com>
2014-12-25 18:09:47 +0800

    commit it all.

---
+++ examples/sWaterfall.js
@@ -0,0 +1,23 @@
+/**
+ * Created by schoeu on 2014/12/18.
+ *
+ * {
+ *
+ * }
+ *
+ */
+/*
+    $(function($){
+        $.fn.extend({
+            sWaterfall : function(options){
+                var contanin = this;
+                var columnWidth,minColumnCount,resNum;
+                if(options){
+                    columnWidth = options.columnWidth;
+                    minColumnCount = options.minColumnCount;
+                    resColNum = Math.min(Math.ceil(contanin.width()/columnWidth),minColumnCount) || 2;
+                }
+            }
+        });
+    })(jQuery);
+*/
